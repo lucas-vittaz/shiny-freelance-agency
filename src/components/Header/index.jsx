0638b5b050408f40588230841729e5abcb79dc8a
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-08 17:45:28 +0200

    adding freelance linkto

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
 
 function Header() {
-    return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/survey">Questionnaire</Link>
-        </nav>
-    )
+  return (
+    <nav>
+      <Link to="/">Accueil</Link>
+      <Link to="/survey/1">Questionnaire</Link>
+      <Link to="/freelances">Profils</Link>
+    </nav>
+  )
 }
 
 export default Header
