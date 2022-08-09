16e7bd0dc53c5e6f7a0e4842d9d73a1d8b53b51f
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-09 13:43:29 +0200

    import card component into freelance

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,9 +1,36 @@
+import DefaultPicture from '../../assets/profile.png'
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'Devops',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Developpeur frontend',
+        picture: DefaultPicture,
+    },
+    {
+        name: 'Jean neige',
+        jobTitle: 'Développeur Fullstack',
+        picture: DefaultPicture,
+    },
+]
+
 function Freelances() {
   return (
-    <div>
-      <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
-    </div>
+      <div>
+          <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+          {freelanceProfiles.map((profile, index) => (
+              <Card
+                  key={`${profile.name}-${index}`}
+                  label={profile.jobTitle}
+                  picture={profile.picture}
+                  title={profile.name}
+              />
+          ))}
+      </div>
   )
 }
-
 export default Freelances
