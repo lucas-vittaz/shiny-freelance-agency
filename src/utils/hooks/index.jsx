8640277e286226b3d7bea86517a518a8fc2362c0
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-11 17:21:02 +0200

    homemade hook

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -1,37 +1,32 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { ThemeContext } from '../context'
 
 export function useFetch(url) {
+  const [data, setData] = useState({})
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
-const [data, setData] = useState({})
-
-const [isLoading, setLoading] = useState(true)
-
-
-
-useEffect(() => {
-
-if (!url) return
-
-async function fetchData() {
-
-const response = await fetch(url)
-
-const data = await response.json()
-
-setData(data)
-
-setLoading(false)
-
+  useEffect(() => {
+    if (!url) return
+    setLoading(true)
+    async function fetchData() {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setData(data)
+      } catch (err) {
+        console.log(err)
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [url])
+  return { isLoading, data, error }
 }
 
-setLoading(true)
-
-fetchData()
-
-}, [url])
-
-
-
-return { isLoading, data }
-
+export function useTheme() {
+  const { theme, toggleTheme } = useContext(ThemeContext)
+  return { theme, toggleTheme }
 }
