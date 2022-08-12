8b8c98cd8297d8f1f1adf9a170aec91610e63afa
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-08-12 12:41:50 +0200

    second test

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,6 +1,12 @@
 import { formatJobList } from './'
 
-test('Ceci est mon premier test', () => {
-    const expectedState = 'item2,'
-    expect(formatJobList('item2', 3, 1)).toEqual(expectedState)
+describe('La fonction formatJobList', () => {
+    test('ajoute une virgule à un item', () => {
+        const expectedState = 'item2,'
+        expect(formatJobList('item2', 3, 1)).toEqual(expectedState)
+    })
+    test('ne met pas de virgule pour le dernier élément', () => {
+        const expectedState = 'item3'
+        expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
+    })
 })
